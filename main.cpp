0759e1c6fcf48753d160dd4ab910bc31dd2709e6
vellw <vellw1002@gmail.com>
2018-10-12 09:33:59 +0800

    更新

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include "mainwindow.hpp"
 #include <QApplication>
+#include <QSplashScreen>
+#include <QPixmap>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QPixmap pixmap( ":/png/splash/img/mysqlgui-blue.png" );
+
+    QSplashScreen splash( pixmap );
+    splash.show();
+
+    a.processEvents();
+
     MainWindow w;
     w.show();
 
+    splash.finish(&w);
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.hpp"
 #include "ui_mainwindow.h"
+#include <QDateTime>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QDateTime n=QDateTime::currentDateTime();
+    QDateTime now;
+    do{
+        now=QDateTime::currentDateTime();
+        QCoreApplication::processEvents();
+    } while (n.secsTo(now)<=5);//5为需要延时的秒数
 }
 
 MainWindow::~MainWindow()
